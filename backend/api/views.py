0a3bc7e6cbdf9523dee84c8f75d7efbf7306ded7
FarzamAliKhan <farzam909@gmail.com>
2023-10-16 03:18:00 +0500

    Did a bunch of api stuff to get fundamentals of api right

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+def api_home(request, *args, **kwargs):
+    print(request.GET)   #echo params which are https://128.0.1.8000/?arg=value  last parts are query params
+    print(request.POST)     #echo forms/post
+    body = request.body      #byte string of JSON data
+    data = {}               #converting byte string into python dictionary
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    #print(request.headers)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,10 @@
 import requests
 
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
    
-get_Request = requests.get(endpoint)
-print(get_Request.text)
-print(get_Request.status_code)
+get_Request = requests.get(endpoint, params={"abc":123}, json={"query":"hello world"})
+#print(get_Request.text)
+#print(get_Request.status_code)
+print(get_Request.json())
+
